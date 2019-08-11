93462d5ca084a757d224b33a24a6b9c1599d2b14
david <davidndy@gmail.com>
2019-08-10 23:09:32 -0500

    heap inherit array_b_t  and fix some bugs

--- graphs_algorithms_and_data_structures/chapter_10/array_as_b_tree.rb
+++ graphs_algorithms_and_data_structures/chapter_10/array_as_b_tree.rb
@@ -4,7 +4,7 @@ class Arr_b_tree
   end
 
   def add(element)
-    @a_b_t.push(element)
+    self.array.push(element)
   end
 
   def childrens(parent)
@@ -14,43 +14,43 @@ class Arr_b_tree
   def children_a(parent)
     children_pos = parent + parent + 1
 
-    [@a_b_t[children_pos]]
+    [self.array[children_pos]]
   end
 
   def children_b(parent)
     children_pos = parent + parent + 2
 
-    [@a_b_t[children_pos]]
+    [self.array[children_pos]]
   end
 
   def parent(children)
-    return [@a_b_t[0]] if children == 0
+    return [self.array[0]] if children == 0
 
     factor = children.odd? ? (children / 2) : (children / 2) - 1
 
     position = children - (children - factor)
 
-    [@a_b_t[position]]
+    [self.array[position]]
   end
 end
-
-a_b_t = Arr_b_tree.new
-p a_b_t
-a_b_t.add(0)
-a_b_t.add(1)
-a_b_t.add(2)
-a_b_t.add(3)
-a_b_t.add(4)
-a_b_t.add(5)
-a_b_t.add(6)
-a_b_t.add(7)
-
-p a_b_t
-node = 1
-p a_b_t.children_a(node)
-p a_b_t.children_b(node)
- p a_b_t.childrens(node)
-nodes = 8.times.map do|num|
-  p num
-  p a_b_t.parent(num)
-end
+#
+#a_b_t = Arr_b_tree.new
+#p a_b_t
+#a_b_t.add(0)
+#a_b_t.add(1)
+#a_b_t.add(2)
+#a_b_t.add(3)
+#a_b_t.add(4)
+#a_b_t.add(5)
+#a_b_t.add(6)
+#a_b_t.add(7)
+#
+#p a_b_t
+#node = 1
+#p a_b_t.children_a(node)
+#p a_b_t.children_b(node)
+# p a_b_t.childrens(node)
+#nodes = 8.times.map do|num|
+#  p num
+#  p a_b_t.parent(num)
+#end
---
+++ graphs_algorithms_and_data_structures/chapter_10/test.rb
@@ -0,0 +1,5 @@
+class Foo
+  def sum
+    2+2
+  end
+end
